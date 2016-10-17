442e747aa83a25c9663c202a45720ee7aae3f5b0
Remco <caseda08@hotmail.com>
2016-10-17 21:35:05 +0200

    add Basic CC into on/off capability for physical state changes.

--- drivers/TZ65/driver.js
+++ drivers/TZ65/driver.js
@@ -10,22 +10,29 @@ const ZwaveDriver = require('homey-zwavedriver');
 
 module.exports = new ZwaveDriver( path.basename(__dirname), {
 	capabilities: {
-		'onoff': {
-			'command_class': 'COMMAND_CLASS_SWITCH_MULTILEVEL',
-			'command_get': 'SWITCH_MULTILEVEL_GET',
-			'command_set': 'SWITCH_MULTILEVEL_SET',
-			'command_set_parser': value => {
-				return {
-					'Value': (value > 0) ? 'on/enable' : 'off/disable'
-				};
+		'onoff': [
+			{
+				'command_class': 'COMMAND_CLASS_SWITCH_MULTILEVEL',
+				'command_get': 'SWITCH_MULTILEVEL_GET',
+				'command_set': 'SWITCH_MULTILEVEL_SET',
+				'command_set_parser': value => {
+					return {
+						'Value': (value > 0) ? 'on/enable' : 'off/disable'
+					};
+				},
+				'command_report': 'SWITCH_MULTILEVEL_REPORT',
+				'command_report_parser': report => {
+					if (typeof report['Value'] === 'string') return report['Value'] === 'on/enable';
+					
+					return report['Value (Raw)'][0] > 0;
+				}
 			},
-			'command_report': 'SWITCH_MULTILEVEL_REPORT',
-			'command_report_parser': report => {
-				if (typeof report['Value'] === 'string') return report['Value'] === 'on/enable';
-				
-				return report['Value (Raw)'][0] > 0;
+			{
+				'command_class': 'COMMAND_CLASS_BASIC',
+				'command_report': 'BASIC_REPORT',
+				'command_report_parser': report => report['Value' ] > 0
 			}
-		},
+		],
 		
 		'dim': {
 			'command_class': 'COMMAND_CLASS_SWITCH_MULTILEVEL',
