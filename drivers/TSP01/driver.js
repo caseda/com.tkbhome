1a7d7c3e68490fb6f1440c58b98738d6ee02938e
Remco <caseda08@hotmail.com>
2016-12-30 23:17:47 +0100

    last big update to TSP01 driver

--- drivers/TSP01/driver.js
+++ drivers/TSP01/driver.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const ZwaveDriver = require('homey-zwavedriver');
-let basicSetMode = true;
 
 // TSM02/TSP01
 // TSM02 Product Page => http://www.tkbhome.com/?cn-p-d-257.html
@@ -15,11 +14,9 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		'alarm_contact': {
 			'command_class': 'COMMAND_CLASS_SENSOR_BINARY',
 			'command_get': 'SENSOR_BINARY_GET',
-			'command_get_parser': () => {
-				return {
-					'Sensor Type': 'Door/Window'
-				};
-			},
+			'command_get_parser': () => ({
+				'Sensor Type': 'Door/Window'
+			}),
 			'command_report': 'SENSOR_BINARY_REPORT',
 			'command_report_parser': report => {
 				if (report['Sensor Type'] === 'Door/Window')
@@ -27,33 +24,30 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 					
 				return null;
 			},
-			'optional': true
+			'optional': true,
 		},
 		
 		'alarm_motion': [
 			{
 				'command_class': 'COMMAND_CLASS_SENSOR_BINARY',
 				'command_get': 'SENSOR_BINARY_GET',
-				'command_get_parser': () => {
-					return {
-						'Sensor Type': 'Motion'
-					};
-				},
+				'command_get_parser': () => ({
+					'Sensor Type': 'Motion',
+				}),
 				'command_report': 'SENSOR_BINARY_REPORT',
 				'command_report_parser': report => {
 					if (report['Sensor Type'] === 'Motion')
 						return report['Sensor Value'] === 'detected an event';
 						
 					return null;
-				},
-				'poll_interval': 'poll_interval'
+				}
 			},
 			{
 				'command_class': 'COMMAND_CLASS_BASIC',
 				'command_report': 'BASIC_SET',
 				'command_report_parser': report => {
-					if (basicSetMode === true)
-						return report.Value > 0;
+					if (report.Value === 0)
+						return false;
 						
 					return null;
 				}
@@ -61,14 +55,12 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		],
 		
 		'alarm_tamper': {
-		'command_class': 'COMMAND_CLASS_SENSOR_BINARY',
-		'command_get': 'SENSOR_BINARY_GET',
-		'command_get_parser': () => {
-			return {
-				'Sensor Type': 'Tamper'
-			};
-		},
-		'command_report': 'SENSOR_BINARY_REPORT',
+			'command_class': 'COMMAND_CLASS_SENSOR_BINARY',
+			'command_get': 'SENSOR_BINARY_GET',
+			'command_get_parser': () => ({
+				'Sensor Type': 'Tamper',
+			}),
+			'command_report': 'SENSOR_BINARY_REPORT',
 			'command_report_parser': report => {
 				if (report['Sensor Type'] === 'Tamper')
 					return report['Sensor Value'] === 'detected an event';
@@ -79,10 +71,26 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		
 		'measure_luminance': {
 			'command_class': 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			'command_get': 'SENSOR_MULTILEVEL_GET',
+			'command_get_parser': () => ({
+				'Sensor Type': 'Luminance (version 1)',
+				'Properties1': {
+					'Scale': 1,
+				},
+			}),
 			'command_report': 'SENSOR_MULTILEVEL_REPORT',
 			'command_report_parser': report => {
-				if (report['Sensor Type'] === 'Luminance (version 1)')
-					return report['Sensor Value (Parsed)'];
+				if (report['Sensor Type'] === "Luminance (version 1)" &&
+				report.hasOwnProperty("Level") &&
+				report.Level.hasOwnProperty("Scale")) {
+					if (report.Level.Scale === 1)
+						return report['Sensor Value (Parsed)'];
+						
+					if (report.Level.Scale === 0)
+						return report['Sensor Value (Parsed)'] * 5;
+						
+					return null;
+				}
 					
 				return null;
 			}
@@ -90,23 +98,42 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		
 		'measure_temperature': {
 			'command_class': 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			'command_get': 'SENSOR_MULTILEVEL_GET',
+			'command_get_parser': () => ({
+				'Sensor Type': 'Temperature (version 1)',
+				'Properties1': {
+					'Scale': 0,
+				},
+			}),
 			'command_report': 'SENSOR_MULTILEVEL_REPORT',
 			'command_report_parser': report => {
-				if (report['Sensor Type'] === 'Temperature (version 1)')
-					return report['Sensor Value (Parsed)']);
+				if (report['Sensor Type'] === "Temperature (version 1)" &&
+				report.hasOwnProperty("Level") &&
+				report.Level.hasOwnProperty("Scale")) {
+					if (report.Level.Scale === 0)
+						return report['Sensor Value (Parsed)'];
 					
+					if (report.Level.Scale === 1)
+						return (report['Sensor Value (Parsed)'] - 32) / 1.8;
+					
+					return null;
+				}
+				
 				return null;
 			}
 		},
 		
 		'measure_battery': {
 			'command_class': 'COMMAND_CLASS_BATTERY',
-			'command_get': 'BATTERY_GET',
 			'command_report': 'BATTERY_REPORT',
 			'command_report_parser': report => {
-				if (report['Battery Level'] === 'battery low warning') return 1;
+				if (report['Battery Level'] === 'battery low warning')
+					return 1;
 				
-				return report['Battery Level (Raw)'][0];
+				if (report.hasOwnProperty('Battery Level (Raw)'))
+					return report['Battery Level (Raw)'][0];
+					
+				return null;
 			}
 		}
 	},
@@ -114,6 +141,7 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		"basic_set_level": {
 			"index": 2,
 			"size": 1,
+			"signed": false,
 		},
 		"pir_sensitivity": {
 			"index": 3,
@@ -126,19 +154,17 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		"test_mode": {
 			"index": 5,
 			"size": 1,
-			"parser": (value, newSettings) => {
-				let param5 = 0;
+			"parser": (value, settings) => {
+				// Operation mode bit 0 (0000000x)
+				let param5 = Math.round(param5 + Number(settings.operation_mode));
 				
-				//If operation_mode = Security Mode, use 1 for bit 0 (00000001)
-				param5 = Math.round(param5 + Number(newSettings.operation_mode));
-				
-				//If test_mode is true, use 1 for bit 1 (00000010)
+				// Operation mode bit 1 (000000x0)
 				if (value) {
 					Math.round(param5 = param5 + 2);
 				}
 				
-				//If door/window_mode is false, use 1 for bit 2 (00000100)
-				if (newSettings["door/window_mode"]) {
+				// Operation mode bit 2 (00000x00)
+				if (settings["door/window_mode"]) {
 					Math.round(param5 = param5 + 4);
 				}
 				
@@ -148,19 +174,17 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		"operation_mode": {
 			"index": 5,
 			"size": 1,
-			"parser": (value, newSettings) => {
-				let param5 = 0;
+			"parser": (value, settings) => {
+				// Operation mode bit 0 (0000000x)
+				let param5 = Math.round(param5 + Number(value));
 				
-				//If operation_mode = Security Mode, use 1 for bit 0 (00000001)
-				param5 = Math.round(param5 + Number(value));
-				
-				//If test_mode is true, use 1 for bit 1 (00000010)
-				if (newSettings.test_mode) {
+				// Operation mode bit 1 (000000x0)
+				if (settings.test_mode) {
 					Math.round(param5 = param5 + 2);
 				}
 				
-				//If door/window_mode is false, use 1 for bit 2 (00000100)
-				if (newSettings["door/window_mode"]) {
+				// Operation mode bit 2 (00000x00)
+				if (settings["door/window_mode"]) {
 					Math.round(param5 = param5 + 4);
 				}
 				
@@ -170,18 +194,16 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		"door/window_mode": {
 			"index": 5,
 			"size": 1,
-			"parser": (value, newSettings) => {
-				let param5 = 0;
+			"parser": (value, settings) => {
+				// Operation mode bit 0 (0000000x)
+				let param5 = Math.round(param5 + Number(settings.operation_mode));
 				
-				//If operation_mode = Security Mode, use 1 for bit 0 (00000001)
-				param5 = Math.round(param5 + Number(newSettings.operation_mode));
-				
-				//If test_mode is true, use 1 for bit 1 (00000010)
-				if (newSettings.test_mode) {
+				// Operation mode bit 1 (000000x0)
+				if (settings.test_mode) {
 					Math.round(param5 = param5 + 2);
 				}
 				
-				//If door/window_mode is true, use 1 for bit 2 (00000100)
+				// Operation mode bit 2 (00000x00)
 				if (value) {
 					Math.round(param5 = param5 + 4);
 				}
@@ -204,6 +226,11 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			"size": 1,
 			"parser": value => new Buffer([Math.round(value * 2)]),
 		},
+		"contact_report_time": {
+			"index": 11,
+			"size": 1,
+			"parser": value => new Buffer([Math.round(value * 2)]),
+		},
 		"illumination_report_time": {
 			"index": 12,
 			"size": 1,
@@ -216,36 +243,3 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		},
 	}
 });
-		
-module.exports.on('initNode', token => {
-	const node = module.exports.nodes[token];
-
-	if (node) {
-		// Initial basicSetMode on start of app
-		module.exports.getSettings(node.device_data, (err, settings) => {
-			if (!err &&
-			settings &&
-			settings.hasOwnProperty('motion_interact_class_basic')) {
-				basicSetMode = settings.motion_interact_class_basic;
-			}
-		});
-	}
-});
-
-// Monkeypatch settings, so we can remove our driver-specific element from the changedKeysArr
-module.exports._updateSettings = module.exports.settings;
-module.exports.settings = (deviceData, newSettingsObj, oldSettingsObj, changedKeysArr, callback) => {
-	const changedKeys = [];
-	for (var i = 0; i < changedKeysArr.length; i++) {
-		if (changedKeysArr[i] === 'motion_interact_class_basic') continue;
-		changedKeys.push(changedKeysArr[i]);
-	}
-	
-	// Update basicSetMode variable if needed
-	if (changedKeysArr.indexOf('motion_interact_class_basic') > 0 &&
-	basicSetMode !== newSettingsObj['motion_interact_class_basic']) {
-		basicSetMode = newSettingsObj['motion_interact_class_basic'];
-	}
-	
-	module.exports._updateSettings(deviceData, newSettingsObj, oldSettingsObj, changedKeys, callback);
-}
