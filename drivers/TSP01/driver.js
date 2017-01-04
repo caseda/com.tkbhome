bf254ea1e8695f62a382b6b68cd2220dfe4d9e82
Remco <caseda08@hotmail.com>
2017-01-04 16:54:44 +0100

    fix things i overlooked/forgot

--- drivers/TSP01/driver.js
+++ drivers/TSP01/driver.js
@@ -46,7 +46,12 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		],
 		
 		'alarm_tamper': {
+			'getOnWakeUp': true,
 			'command_class': 'COMMAND_CLASS_SENSOR_BINARY',
+			'command_get': 'SENSOR_BINARY_GET',
+			'command_get_parser': () => ({
+				'Sensor Type': 'Tamper'
+			}),
 			'command_report': 'SENSOR_BINARY_REPORT',
 			'command_report_parser': report => {
 				if (report['Sensor Type'] === 'Tamper')
@@ -60,6 +65,12 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			'getOnWakeUp': true,
 			'command_class': 'COMMAND_CLASS_SENSOR_MULTILEVEL',
 			'command_get': 'SENSOR_MULTILEVEL_GET',
+			'command_get_parser': () => ({
+				'Sensor Type': 'Luminance (version 1)',
+				'Properties1': {
+					'Scale': 0,
+				},
+			}),
 			'command_report': 'SENSOR_MULTILEVEL_REPORT',
 			'command_report_parser': (report, node) => {
 				if (report['Sensor Type'] === 'Luminance (version 1)') {
@@ -103,9 +114,7 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		},
 		
 		'measure_battery': {
-			'getOnWakeUp': true,
 			'command_class': 'COMMAND_CLASS_BATTERY',
-			'command_get': 'BATTERY_GET',
 			'command_report': 'BATTERY_REPORT',
 			'command_report_parser': report => {
 				if (report['Battery Level'] === 'battery low warning')
